a49b9a9f01418d62987932a64fc1ee0772f24f83
prncsahu022 <prncsahu022@gmail.com>
2019-10-23 20:34:21 +0530

    Two XML files named "edit account" and "edit account preview" same layout in both but "edit account preview" have textView rather then Editext.

---
+++ app/src/main/java/com/android/example/baki_bohi/EditAccountPreview.java
@@ -0,0 +1,14 @@
+package com.android.example.baki_bohi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditAccountPreview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_account_preview);
+    }
+}
